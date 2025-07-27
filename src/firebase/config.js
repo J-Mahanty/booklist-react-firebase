b31f2c9c4e4f1b0ab7f989f7f562df665b14f3fd
J-Mahanty <jayatimahanty@gmail.com>
2025-07-27 10:22:41 +0530

    Firebase installed

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-OamP8DMXuConHduypWCtcvbynqiWrTc",
+  authDomain: "fir-book-list-ed0e4.firebaseapp.com",
+  projectId: "fir-book-list-ed0e4",
+  storageBucket: "fir-book-list-ed0e4.firebasestorage.app",
+  messagingSenderId: "458279618982",
+  appId: "1:458279618982:web:72fe7c0a1591fe111fa767"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
