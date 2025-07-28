10b551c48da7d539f4a0c175def6071751e7dcd4
J-Mahanty <jayatimahanty@gmail.com>
2025-07-28 13:24:03 +0530

    persistnat log in added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import BooksPage from './views/BooksPage.jsx';
 import SingleBookPage from './views/SingleBookPage.jsx';
 import LoginPage from './views/LoginPage.jsx';
 import AddBookPage from './views/AddBookPage.jsx';
+import { selectUsers } from './store/UsersSlice.js';
+import { useSelector } from 'react-redux';
 
 function App() {
 
+  const user = useSelector(selectUsers);
+
   return (
     <>  
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path="add-book" element={<AddBookPage />} />
-          <Route path="book/:id" element={<SingleBookPage />} />
-        </Routes>
-      </BrowserRouter>
-      
-        
+
+      { user.currentUser? 
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<BooksPage />} />
+            <Route path="add-book" element={<AddBookPage />} />
+            <Route path="book/:id" element={<SingleBookPage />} />
+          </Routes>
+        </BrowserRouter>
+        :
+        <LoginPage/>
+      }
+       
     </>
   )
 }
--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -1,17 +1,30 @@
 import FullPageLoader from '../components/FullPageLoader.jsx';
 import {useState} from 'react';
 import { auth } from '../firebase/config.js';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../store/UsersSlice.js';
 import { 
   createUserWithEmailAndPassword, 
   signInWithEmailAndPassword,
-  sendPasswordResetEmail 
+  sendPasswordResetEmail,
+  onAuthStateChanged 
 } from 'firebase/auth';
 
 function LoginPage() {
+
+  const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
   const [loginType, setLoginType] = useState('login');
   const [error, setError] = useState('');
   const [userCredentials, setUserCredentials] = useState();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      dispatch(setUser({id: user.uid, email: user.email}));
+    } else {
+      dispatch(setUser(null)); 
+    }
+  })
 
   function handleCredentials(e){
     setUserCredentials({...userCredentials, [e.target.name]: e.target.value});
@@ -23,8 +36,7 @@ function LoginPage() {
 
     createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
     .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
+      dispatch(setUser({id: userCredential.user.uid, email: userCredential.user.email}));
     })
     .catch((error) => {
       setError(error.message);
@@ -37,7 +49,7 @@ function LoginPage() {
 
     signInWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
     .then((userCredential) => {
-      console.log(userCredential.user);
+      dispatch(setUser({id: userCredential.user.uid, email: userCredential.user.email}));
     })
     .catch((error) => {
       setError(error.message);
@@ -47,62 +59,62 @@ function LoginPage() {
   function handlePasswordReset(){
     const email = prompt('Please enter your email');
     sendPasswordResetEmail(auth, email);
-    alert('Email sent! Check your inbox for instructions to reset your password')
+    alert('Email sent! Check your inbox or spam and junk folder for instructions to reset your password')
   }
 
   
 
   
-    return (
-      <>
-        { isLoading && <FullPageLoader></FullPageLoader> }
-        
-        <div className="container login-page">
-          <section>
-            <h1>Welcome to the Book App</h1>
-            <p>Login or create an account to continue</p>
-            <div className="login-type">
-              <button 
-                className={`btn ${loginType == 'login' ? 'selected' : ''}`}
-                onClick={()=>setLoginType('login')}>
-                  Login
-              </button>
-              <button 
-                className={`btn ${loginType == 'signup' ? 'selected' : ''}`}
-                onClick={()=>setLoginType('signup')}>
-                  Signup
-              </button>
-            </div>
-            <form className="add-form login">
-                  <div className="form-control">
-                      <label>Email *</label>
-                      <input onChange={(e) => {handleCredentials(e)}} type="text" name="email" placeholder="Enter your email" />
-                  </div>
-                  <div className="form-control">
-                      <label>Password *</label>
-                      <input onChange={(e) => {handleCredentials(e)}} type="password" name="password" placeholder="Enter your password" />
-                  </div>
-                  {
-                    loginType == 'login' ?
-                    <button onClick={(e) => {handleLogin(e)}} className="active btn btn-block">Login</button>
-                    : 
-                    <button onClick={(e) => {handleSignUp(e)}} className="active btn btn-block">Sign Up</button>
-                  }
+  return (
+    <>
+      { isLoading && <FullPageLoader></FullPageLoader> }
+      
+      <div className="container login-page">
+        <section>
+          <h1>Welcome to the Book App</h1>
+          <p>Login or create an account to continue</p>
+          <div className="login-type">
+            <button 
+              className={`btn ${loginType == 'login' ? 'selected' : ''}`}
+              onClick={()=>setLoginType('login')}>
+                Login
+            </button>
+            <button 
+              className={`btn ${loginType == 'signup' ? 'selected' : ''}`}
+              onClick={()=>setLoginType('signup')}>
+                Signup
+            </button>
+          </div>
+          <form className="add-form login">
+                <div className="form-control">
+                    <label>Email *</label>
+                    <input onChange={(e) => {handleCredentials(e)}} type="text" name="email" placeholder="Enter your email" />
+                </div>
+                <div className="form-control">
+                    <label>Password *</label>
+                    <input onChange={(e) => {handleCredentials(e)}} type="password" name="password" placeholder="Enter your password" />
+                </div>
+                {
+                  loginType == 'login' ?
+                  <button onClick={(e) => {handleLogin(e)}} className="active btn btn-block">Login</button>
+                  : 
+                  <button onClick={(e) => {handleSignUp(e)}} className="active btn btn-block">Sign Up</button>
+                }
 
-                  {error &&
+                {error &&
 
-                  <div className="error">{error}</div>
+                <div className="error">{error}</div>
 
-                  }
+                }
 
-                  <p onClick={handlePasswordReset} className="forgot-password">Forgot Password?</p>
-                  
-              </form>
-          </section>
-        </div>
-      </>
-    )
-  }
-  
-  export default LoginPage
+                <p onClick={handlePasswordReset} className="forgot-password">Forgot Password?</p>
+                
+            </form>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default LoginPage
   
